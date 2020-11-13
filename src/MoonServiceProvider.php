3b3587f93119c406e43afb8bf936008883f1aed4
JeroenvanRensen <jmjrensen@jcjcj.nl>
2020-11-13 09:50:11 +0100

    Create a service provider

---
+++ src/MoonServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace JeroenvanRensen\MoonPHP;
+
+use Illuminate\Support\ServiceProvider;
+
+class MoonServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // 
+    }
+
+    public function boot()
+    {
+        // 
+    }
+}
