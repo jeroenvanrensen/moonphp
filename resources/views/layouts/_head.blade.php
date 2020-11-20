622c9f42eeaaff71c12c30374a6ed874f44a6e42
JeroenvanRensen <jmjrensen@jcjcj.nl>
2020-11-20 14:19:43 +0100

    Install Laravel Livewire

--- resources/views/layouts/_head.blade.php
+++ resources/views/layouts/_head.blade.php
@@ -1,6 +1,10 @@
+<!-- Metas -->
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
+<!-- Title -->
 <title>@yield('title') - MoonPHP</title>
 
+<!-- Styles -->
 <link href="{{ asset('moonphp/css/app.css') }}" rel="stylesheet" />
+@livewireStyles
