0368a7340b86a5beed41f6c66ae8bbf931568e1e
JeroenvanRensen <jmjrensen@jcjcj.nl>
2020-11-20 12:51:25 +0100

    Extract layouts partials

---
+++ resources/views/layouts/_head.blade.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<title>@yield('title') - MoonPHP</title>
+
+<link href="{{ asset('moonphp/css/app.css') }}" rel="stylesheet" />
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
-    <title>@yield('title') - MoonPHP</title>
-
-    <link href="{{ asset('moonphp/css/app.css') }}" rel="stylesheet" />
+    @include('moon::layouts._head')
 </head>
 <body class="bg-gray-200">
     <div class="flex">
@@ -21,6 +16,6 @@
         </main>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    @include('moon::layouts._scripts')
 </body>
 </html>
